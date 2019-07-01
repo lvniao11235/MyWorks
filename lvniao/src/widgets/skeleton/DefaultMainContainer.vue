517a78ab6200a909f93b61a6593d2b9eb580bb74
JoeJiao <lvniao11235@163.com>
2019-07-01 13:35:25 +0800

    add slipe for tabcontainer

--- lvniao/src/widgets/skeleton/DefaultMainContainer.vue
+++ lvniao/src/widgets/skeleton/DefaultMainContainer.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="lv-widget">
-        <div class="lv-tab-head lv-skin-border-primary">
-            <div v-for="tab in tabs" :class="selectedClass(tab)" @click="selectedTab(tab)" :key="tab.id" class="lv-tab-item">
+        <div class="lv-tab-head lv-skin-border-primary" ref="tabContainer">
+            <div v-for="tab in tabs" :class="selectedClass(tab)" @click="selectedTab(tab)" :id="tab.id" :key="tab.id" class="lv-tab-item">
                 <span :class="tab.icon"></span>
                 {{tab.text}}
+                <span class="fa fa-close" @click.stop="closeTab(tab)" v-if="tab.id != 1"></span>
+            </div>
+            <div class="lv-tab-btns lv-skin-border-primary">
+                <span class="lv-tab-btn lv-skin-border-primary fa fa-angle-left" @click.stop="moveToLeft"></span>
+                <span class="lv-tab-btn lv-skin-border-primary fa fa-close" @click.stop="closeAll"></span>
+                <span class="lv-tab-btn lv-skin-border-primary fa fa-angle-right" @click.stop="moveToRight"></span>
             </div>
         </div>
         <div class="lv-tab-container">
@@ -20,7 +26,9 @@ import {mapState, mapMutations} from 'vuex';
 export default{
     data:function(){
         return {
-            tabs:[]
+            tabs:[],
+            tabsWidth:0,
+            marginleft:0
         }
     },
     computed:{
@@ -34,9 +42,31 @@ export default{
         openNewTabHandle(item){
             if(!this.tabs.find(x=>x.id == item.id)){
                 this.tabs.push(item);
+                this.$nextTick(function(){
+                    this.resetTabContainerWidth();
+                });
             }
             this.$store.commit("changeCurrentTab", item);
             this.$router.push(item.url);
+            this.$nextTick(function(){
+                if(this.tabsWidth > (this.$refs.tabContainer.clientWidth - 62 - 10)){
+                    var marginleft = 0
+                    for(var i=0; i<this.$refs.tabContainer.childElementCount;i++){
+                        if(this.$refs.tabContainer.children[i].id.toString() == item.id.toString()){
+                            break;
+                        }
+                        marginleft += this.$refs.tabContainer.children[i].clientWidth + 7;
+                    }
+                    if(marginleft > this.$refs.tabContainer.clientWidth - 200){
+                        this.marginleft = this.$refs.tabContainer.clientWidth - 200 - marginleft;
+                    } else {
+                        this.marginleft = 0;
+                    }
+                    this.$refs.tabContainer.children[0].style.marginLeft = this.marginleft + "px";
+                }
+                
+            });
+            
         },
         selectedClass(tab){
             return tab.id == this.currentTab.id ? "lv-skin-border-primary lv-selected":"";
@@ -44,11 +74,77 @@ export default{
         selectedTab(tab){
             this.$store.commit("changeCurrentTab", tab);
             this.$router.push(tab.url);
+        },
+        closeTab(tab){
+            if(tab.id == 1) return;
+            var index = this.tabs.findIndex(x=>x.id == tab.id);
+            this.tabs.splice(index, 1);
+            this.$nextTick(function(){
+                this.resetTabContainerWidth();
+            });
+            if(tab.id != this.currentTab.id) return;
+            if(index == 0){
+                this.$store.commit("changeCurrentTab", this.tabs[index]);
+            } else if(index > 0) {
+                var newtab =  this.tabs[index-1]
+                this.$store.commit("changeCurrentTab",newtab);
+            } else {
+                return;
+            }
+            this.$router.push(this.currentTab.url);
+        },
+        resetTabContainerWidth(){
+            this.tabsWidth = 10;
+            
+            for(var i=0; i<this.$refs.tabContainer.childElementCount; i++){
+                this.tabsWidth += this.$refs.tabContainer.children[i].clientWidth + 3 + 5;
+            }
+        },
+        resizeHandle(){
+            this.$nextTick(function(){
+                this.resetTabContainerWidth();
+                if(this.tabsWidth < this.$refs.tabContainer.clientWidth){
+                    this.marginleft = 0;
+                    this.$refs.tabContainer.children[0].style.marginLeft = this.marginleft + "px";
+                }
+            });
+        },
+        moveToLeft(){
+            this.$refs.tabContainer;
+            if(this.tabsWidth > (this.$refs.tabContainer.clientWidth - 62 - this.marginleft)){
+                this.marginleft += -100;
+                if((this.$refs.tabContainer.clientWidth - 62 - this.marginleft) >= this.tabsWidth){
+                    this.marginleft = -(this.tabsWidth - this.$refs.tabContainer.clientWidth + 62)
+                }
+            }
+            this.$nextTick(function(){
+                this.$refs.tabContainer.children[0].style.marginLeft = this.marginleft + "px";
+            });
+            
+        },
+        moveToRight(){
+            if(this.marginleft < 0){
+                this.marginleft = Math.min(this.marginleft + 100, 0);
+                this.$nextTick(function(){
+                    this.$refs.tabContainer.children[0].style.marginLeft = this.marginleft + "px";
+                });
+            }
+        },
+        closeAll(){
+            var newtabs = [];
+            newtabs.push(this.tabs.find(x=>x.id == 1));
+            this.tabs = newtabs;
+            this.$router.push(this.tabs[0].url);
+            this.marginleft = 0;
+            this.$refs.tabContainer.children[0].style.marginLeft = this.marginleft + "px";
+            this.$store.commit("changeCurrentTab",this.tabs[0]);
         }
     },
     inject:['widgetEventBus'],
     mounted(){
         this.widgetEventBus.$on("openNewTab", this.openNewTabHandle);
+        this.widgetEventBus.$on("resize", this.resizeHandle);
+
     }
 }
 </script>
@@ -61,6 +157,8 @@ export default{
     background-color:#fff;
     padding-left:10px;
     padding-top:5px;
+    position:relative;
+    overflow:hidden;
 }
 
 .lv-tab-item{
@@ -72,6 +170,7 @@ export default{
     border:1px solid #d2d6de;
     display:inline-block;
     border-bottom-color:transparent;
+    z-index:0px;
 }
 
 .lv-tab-item.lv-selected{
@@ -82,5 +181,31 @@ export default{
     position:relative;
     width:100%;
     height:calc(100% - 36px);
+}
+
+.lv-tab-btns{
+    width:62px;
+    height:30px;
+    border:1px solid;
+    position:absolute;
+    right:10px;
+    top:5px;
+    z-index:5;
+    background-color:#fff;
+}
+
+.lv-tab-btn{
+    width:20px;
+    display:inline-block;
+    text-align:center;
+    height:30px;
+    line-height:30px !important;
+    font-size:18px !important;
+    vertical-align:middle;
+    border-left:1px solid;
+}
+
+.lv-tab-btn:first-child{
+    border-left:none;
 }
 </style>
